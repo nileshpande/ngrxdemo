b8a2beed34729717c97380f90b3762ba26e8be9d
Nilesh Pande <np3225@gmail.com>
2023-04-23 18:11:59 +0530

    angular material stepper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+interface Step {
+  label: string;
+  subtext: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'ngrxdemo';
+  steps = [
+    { label: 'Step 1', subtext: 'Step 1 subtext', content: 'Content for Step 1', completed: false, skipped: false, optional: false,active:false },
+    { label: 'Step 2', subtext: 'Step 2 subtext', content: 'Content for Step 2', completed: false, skipped: false, optional: false,active:true },
+    { label: 'Step 3', subtext: 'Step 3 subtext', content: 'Content for Step 3', completed: false, skipped: true, optional: true,active:false },
+    { label: 'Step 4', subtext: 'Step 4 subtext', content: 'Content for Step 4', completed: false, skipped: false, optional: false,active:false },
+  ];
+
+  isLastStep(step: any): boolean {
+    return this.steps.indexOf(step) === this.steps.length - 1;
+  }
+
+  nextStep(): void {
+    const activeStep:any = this.steps.find(step => step.active);
+    const index = this.steps.indexOf(activeStep);
+    if (index < this.steps.length - 1) {
+      this.steps[index].completed = true;
+      this.steps[index + 1].active = true;
+    }
+  }
+
+  changetext(index:any,text:string)
+  {
+    this.steps[index].content = text;
+  }
+
+  skipStep( ): void {
+    // step.skipped = true;
+    // const activeStep:any = this.steps.find(step => step.active);
+    // const index = this.steps.indexOf(activeStep);
+    // if (index < this.steps.length - 1) {
+    //   this.steps[index].completed = true;
+    //   this.steps[index + 1].active = true;
+    // }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddtodoComponent } from './addtodo/addtodo.component';
 import { TodolistComponent } from './todolist/todolist.component';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { TodoEffects } from './ngrx/todo/todo.effects';
 import { todoReducer } from './ngrx/todo/todo.reducers';
-import { StepperModule } from 'primeng/stepper';
+import { StepsModule } from 'primeng/steps';
 import { ButtonModule } from 'primeng/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -24,10 +27,14 @@ import { ButtonModule } from 'primeng/button';
     TodolistComponent
   ],
   imports: [
+    MatStepperModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FormsModule, ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    StepperModule,
+    StepsModule,
     FormsModule, 
     StoreModule.forRoot({ todos: todoReducer }),
     StoreDevtoolsModule.instrument({
