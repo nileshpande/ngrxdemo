6cd66e8d1f707a192754e4b6c73aac9289814e9b
Nilesh Pande <np3225@gmail.com>
2023-04-24 16:24:44 +0530

    mat stepper active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,40 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
-interface Step {
-  label: string;
-  subtext: string;
+interface stepinterface {
+  label: string,
+  subtext:string 
+  completed: boolean, skipped: boolean, optional: boolean,active:boolean
+  formgroup:FormGroup,
+  icon:string
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false }
+  }]
 })
 
 
 export class AppComponent {
   title = 'ngrxdemo';
-  steps = [
-    { label: 'Step 1', subtext: 'Step 1 subtext', content: 'Content for Step 1', completed: false, skipped: false, optional: false,active:false },
-    { label: 'Step 2', subtext: 'Step 2 subtext', content: 'Content for Step 2', completed: false, skipped: false, optional: false,active:true },
-    { label: 'Step 3', subtext: 'Step 3 subtext', content: 'Content for Step 3', completed: false, skipped: true, optional: true,active:false },
-    { label: 'Step 4', subtext: 'Step 4 subtext', content: 'Content for Step 4', completed: false, skipped: false, optional: false,active:false },
-  ];
+  public steps:stepinterface[]; 
+  selectedIndex:number=0;
 
+  constructor( public formbuilder:FormBuilder)
+  {
+    this.steps = [
+      { label: 'Step 1', subtext: 'Step 1 subtext', completed: false, skipped: false, optional: false,active:true ,formgroup: this.formbuilder.group({firstctrl:['',Validators.required]}), icon:"donut_large"},
+      { label: 'Step 2', subtext: 'Step 2 subtext', completed: false, skipped: false, optional: false,active:false,formgroup: this.formbuilder.group({secondctrl:['',Validators.required]}) , icon:"donut_large"},
+      { label: 'Step 3', subtext: 'Step 3 subtext', completed: false, skipped: false, optional: true,active:false,formgroup: this.formbuilder.group({thirdctrl:['',Validators.required]}) , icon:"donut_large"},
+      { label: 'Step 4', subtext: 'Step 4 subtext', completed: false, skipped: false, optional: false,active:false,formgroup: this.formbuilder.group({fourthctrl:['',Validators.required]}) , icon:"donut_large"},
+    ];
+  
+  } 
   isLastStep(step: any): boolean {
     return this.steps.indexOf(step) === this.steps.length - 1;
   }
@@ -29,24 +42,40 @@ export class AppComponent {
   nextStep(): void {
     const activeStep:any = this.steps.find(step => step.active);
     const index = this.steps.indexOf(activeStep);
-    if (index < this.steps.length - 1) {
-      this.steps[index].completed = true;
+    if (index < this.steps.length - 1) { 
+      this.selectedIndex = this.selectedIndex+1;
       this.steps[index + 1].active = true;
+    } 
+  }
+  completenext():void
+  {
+    const activeStep:any = this.steps.find(step => step.active);
+    const index = this.steps.indexOf(activeStep);
+    if (index < this.steps.length - 1) { 
+      this.selectedIndex = this.selectedIndex+1;
+      this.steps[index + 1].active = true;
+      if(this.steps[index].completed)
+      {
+        this.steps[index].icon = "check_circle";
+      } 
     }
+    
   }
 
   changetext(index:any,text:string)
   {
-    this.steps[index].content = text;
+    this.steps[index].subtext = text;
   }
 
   skipStep( ): void {
-    // step.skipped = true;
-    // const activeStep:any = this.steps.find(step => step.active);
-    // const index = this.steps.indexOf(activeStep);
-    // if (index < this.steps.length - 1) {
-    //   this.steps[index].completed = true;
-    //   this.steps[index + 1].active = true;
-    // }
+    
+    const activeStep:any = this.steps.find(step => step.active);
+    const index = this.steps.indexOf(activeStep);
+    if (index < this.steps.length - 1) {
+      this.selectedIndex = this.selectedIndex+1;
+      this.steps[index].skipped = true;
+      this.steps[index + 1].active = true;
+      this.steps[index].icon = "remove_cricle";
+    }
   }
 }
